c875a3ab4e8ea44dddd35fa19593a7feb1e21cd6
BenardBett <kiplangatbett5@gmail.com>
2021-01-24 00:19:43 +0300

    added an url that includes the app urls

--- myawards/urls.py
+++ myawards/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf import settings
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    path(r'^admin/', admin.site.urls),
+    path('', include('week3.urls')),
 ]
