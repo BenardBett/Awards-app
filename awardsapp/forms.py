fc5d9fd7ef39b958d2c445669010ef3db574e709
BenardBett <kiplangatbett5@gmail.com>
2021-01-23 20:20:23 +0300

    added a class for registration

---
+++ awardsapp/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import *
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
