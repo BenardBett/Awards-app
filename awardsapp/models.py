0d9d45d720217dd5dd81ef4769f6ac331ba011cf
BenardBett <kiplangatbett5@gmail.com>
2021-01-23 19:06:28 +0300

    define a method that renders the template index.html

---
+++ awardsapp/models.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models
+from django.dispatch  import receiver
+from django.http import Http404
+from django.core.exceptions import ObjectDoesNotExist
+
+# Create your models here.
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile_pics/')
+    bio = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    class Meta:
+        db_table = 'profile'
+
+    @receiver(post_save, sender=User)
+    def update_create_profile(sender,instance,created, **kwargs):
+        try:
+            instance.profile.save()
+        except ObjectDoesNotExist:
+            Profile.objects.create(user=instance)
+
+
+    def save_profile(self):
+        self.save()
+
---
+++ awardsapp/views.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    projects = Projects.objects.all()
+    context = {
+    "projects":projects,
+    }
+    return render(request, 'index.html', locals())
