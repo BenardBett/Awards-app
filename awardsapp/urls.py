6dd13d0f31565d9f6f4e3bc426e696727b4b5956
BenardBett <kiplangatbett5@gmail.com>
2021-01-24 00:12:15 +0300

    created a url for searching

--- awardsapp/urls.py
+++ awardsapp/urls.py
@@ -13,4 +13,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('accounts/profile/', views.profile, name='profile'),
     path('updateprofile/', views.updateprofile, name='updateprofile'),
+    path('new-project/', views.postproject, name='newproject'),
+    path('project/<id>', views.get_project, name='project'),
+    path('search/', views.search_projects, name='search_projects'),
 ]
