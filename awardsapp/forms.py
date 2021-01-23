e9b2deb7452a8b5afe366a42078503e1af585b4a
BenardBett <kiplangatbett5@gmail.com>
2021-01-23 20:27:39 +0300

    created a class for updating profile

--- awardsapp/forms.py
+++ awardsapp/forms.py
@@ -15,3 +15,9 @@ class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['email']
+        
+        
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image', 'bio']   
