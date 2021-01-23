2f2157fc45e70bb42acca3fa78a9349cfdc6ed4e
BenardBett <kiplangatbett5@gmail.com>
2021-01-23 19:20:37 +0300

    define a method for registration

--- awardsapp/views.py
+++ awardsapp/views.py
@@ -12,3 +12,17 @@ def home(request):
     "projects":projects,
     }
     return render(request, 'index.html', locals())
+
+ef registration(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            form.save()
+            return redirect('login')
+    else:
+        form = RegisterForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'registration/register.html', context)
