382f40f9aa913b1d2d01e7daad7a5e65e57ae707
BenardBett <kiplangatbett5@gmail.com>
2021-01-24 00:35:15 +0300

    created a test for class profile

--- awardsapp/tests.py
+++ awardsapp/tests.py
@@ -4,3 +4,16 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user('langat')
+        cls.profile1 = Profile(profile_pics = 'bab.jpg', bio = 'born in Kenya', user = cls.user)
+
+    def test_instance(cls):
+        cls.assertTrue(isinstance(cls.profile1, Profile))
+
+    def save_method_test(self):
+        self.profile1.save_profile()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)  
