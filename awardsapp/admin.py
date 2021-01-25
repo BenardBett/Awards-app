7529a24984eb797d3af59031a4780bb68e739f24
BenardBett <kiplangatbett5@gmail.com>
2021-01-26 00:27:06 +0300

    debug before deploying to heroku

--- awardsapp/admin.py
+++ awardsapp/admin.py
@@ -1,8 +1,9 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
 
+from __future__ import unicode_literals
 from django.contrib import admin
-from .models import *
+from .models import Profile, Projects
+
 # Register your models here.
+
+admin.site.register(Projects)
 admin.site.register(Profile)
-admin.site.register(Project)
