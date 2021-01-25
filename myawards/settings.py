fb3a568131d82d11497c862cb8d4c1879cdc1cb9
BenardBett <kiplangatbett5@gmail.com>
2021-01-25 18:01:22 +0300

    deployed to heroku

--- myawards/settings.py
+++ myawards/settings.py
@@ -88,8 +88,7 @@ WSGI_APPLICATION = 'myawards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'awards',
         'USER': 'moringa',
         'PASSWORD': 'Access',
