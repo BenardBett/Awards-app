64340441bb3d69b185257d5874f34ed15d172efb
BenardBett <kiplangatbett5@gmail.com>
2021-01-25 10:46:45 +0300

    added a readme file

--- myawards/settings.py
+++ myawards/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+from pathlib import Path
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -110,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -123,3 +126,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+cloudinary.config(
+  cloud_name = "bnnnnn",
+  api_key = "128685598271748",
+  api_secret = "m4yVotUJ-0BJXMIiLXqxGb9mBi0"
+)
